3365644c9e8d937429ff1609b4a4ea5d035de492
Jeff Oliver <jcoliver@email.arizona.edu>
2020-04-08 15:40:22 -0700

    Add FASTA to PHYLIP conversion

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Phylogenetic analyses for southern appalachian arthropod phylogeography work
+# Phylogenetic analyses for southern appalachian arthropod phylogeography work
+
+## Tasks
+1. Identify partitions & models
+    1. Install PartitionFinder 2 and dependencies [http://www.robertlanfear.com/partitionfinder/](http://www.robertlanfear.com/partitionfinder/)
+    2. Convert from FASTA to phylip format
+        + Uses R package ape
+    3. Create configuration files .cfg
+    4. Create bash script to run PartitionFinder for each data set (should be callable via `python <path-to-PartitionFinder.py> <path-to-folder-with-data>`)
+2. Infer trees
+    1. ...
---
+++ fasta-to-phylip.R
@@ -0,0 +1,31 @@
+# Convert FASTA files to phylip format
+# Jeffrey C. Oliver
+# jcoliver@email.arizona.edu
+# 2020-04-08
+
+rm(list = ls())
+
+################################################################################
+library(ape)
+
+### FASTA file copy creation. Should not need to be run
+# text_files <- list.files(path = "data", pattern = "*.txt", full.names = TRUE)
+# # Drop the info files
+# text_files <- grep(pattern = "_info.txt", x = text_files,
+#                     invert = TRUE, value = TRUE)
+# for (one_file in text_files) {
+#   fas_file <- gsub(pattern = ".txt", replacement = ".fas", x = one_file)
+#   file.copy(from = one_file, to = fas_file, overwrite = TRUE)
+# }
+
+# Read all files in data folder with the .fas extension
+fasta_files <- list.files(path = "data", pattern = "*.fas", full.names = TRUE)
+
+# Iterate over list of files, reading as a data frame and exporting as phylip
+for (one_file in fasta_files){
+  phy_filename <- gsub(pattern = ".fas", replacement = ".phy", x = one_file)
+  df <- read.FASTA(file = one_file)
+  write.dna(x = df,
+            file = phy_filename,
+            format = "sequential")
+}
