a49ee20213c12144204486ba8478851a0c17a533
Jeff Oliver <jcoliver@email.arizona.edu>
2020-05-22 16:53:44 -0700

    Add remaining HPC RAxML scripts

---
+++ tree_inference/Odis_mtCo12/Odis_mtCo12-1000-bs-HPC.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Only need to change these five variables
+NUMBS="1000"
+DATASET="Odis_mtCo12"
+MODEL="GTR"
+OUTGROUP="O_striat"
+#ABSPATH="~/Documents/Science/Papers/Appalachian-Phylo/appalachian-phylogeo/tree_inference"
+
+ANALYSISNAME="$NUMBS bootstraps on $DATASET data"
+#DATAPATH="$DATASET"
+SEQFILE="${DATASET}.phy"
+#OUTDIR="${ABSPATH}/${DATASET}/bs-$NUMBS"
+PREFIX="${DATASET}-$NUMBS-bs"
+PARTSFILE="${DATASET}.parts"
+RAXMLPATH="raxmlHPC-PTHREADS-AVX"
+SEED=202005221208
+
+echo "===   START $ANALYSISNAME   ==="
+
+# Check to make sure output directory exists
+# [ ! -d "bs-$NUMBS" ] && eval mkdir "bs-$NUMBS"
+
+# Remove previous RAxML output files
+# eval rm "${NUMBS}-bs/RAxML_*"
+
+# Unclear why `eval` is needed, but script won't work without it
+eval "$RAXMLPATH" -f a -T 8 -p $SEED -x $SEED -s "$SEQFILE" -n "$PREFIX" -m "$MODEL" -"#$NUMBS" -T 6 -o "$OUTGROUP" -q "$PARTSFILE"
+
+echo "===   END $ANALYSISNAME   ==="
